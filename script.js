9dc112af9760c3dc82d5b476c0f38f2f7e2719ab
habibinolimit <nolimithabibi@gmail.com>
2024-06-09 05:12:38 +0700

    upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// function toggleProjectDescription(id) {
+//     var description = document.getElementById(id);
+//     if (description.style.display === "block") {
+//         description.style.display = "none";
+//     } else {
+//         description.style.display = "block";
+//     }
+// }
